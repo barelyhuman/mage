26746326e0072aa9638e5244bc061abddd02646c
Reaper <ahoy@barelyhuman.dev>
2022-08-17 20:56:27 +0530

    add support for layoutEffect and effect

--- core/mage.tsx
+++ core/mage.tsx
@@ -1,10 +1,16 @@
 import { createSubscribeHOC } from '@barelyhuman/mage/_internal'
-import React, { useEffect } from 'react'
+import React, { useEffect, useLayoutEffect } from 'react'
 import { proxy, subscribe } from 'valtio'
 
 export function createState<S extends Record<string, unknown>>(intitial: S) {
   return proxy(intitial)
 }
+
+// decide if inside an SSR environment or not
+const useEffectFunc =
+  typeof window === 'undefined' || 'Deno' in window
+    ? useEffect
+    : useLayoutEffect
 
 export function makeReactive<S extends Record<string, unknown>>(state: S) {
   return function <Props>(
@@ -12,6 +18,6 @@ export function makeReactive<S extends Record<string, unknown>>(state: S) {
       Props & { state?: S; injections?: Record<string, unknown> }
     >
   ) {
-    return createSubscribeHOC(state, Component, subscribe, useEffect)
+    return createSubscribeHOC(state, Component, subscribe, useEffectFunc)
   }
 }
